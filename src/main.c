428c60c401e1579e8246317d2ade29ceeb7ad5f5
MightyCoderX <36588044+MightyCoderX@users.noreply.github.com>
2026-09-10 17:34:21 +0200

    refactor: improve printing and flush stdout after every event

--- src/main.c
+++ src/main.c
@@ -5,21 +5,31 @@
 void on_document_start(void)
 {
     printf("document started\n");
+    fflush(stdout);
 }
 
 void on_open_tag(Element element)
 {
-    printf("element opened: %s\n", element.name);
+    printf("\nelement opened: %s\n", element.name);
+    while (element.attributes != NULL)
+    {
+        printf("  %s = %s\n", element.attributes->name,
+            element.attributes->value);
+        element.attributes = element.attributes->next;
+    }
+    fflush(stdout);
 }
 
 void on_close_tag(Element element)
 {
     printf("element closed: %s\n", element.name);
+    fflush(stdout);
 }
 
 void on_text(char* string)
 {
-    printf("text: %s\n", string);
+    printf("text: \"%s\"\n", string);
+    fflush(stdout);
 }
 
 int main(int argc, char** argv)
